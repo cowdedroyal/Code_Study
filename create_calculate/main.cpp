2256d118cd971d01c7b70c7b3eab2850139c3ade
用户名 <>
2023-03-06 08:41:52 +0800

    new file:   create_calculate/create_calculate.cpp

---
+++ create_calculate/main.cpp
@@ -0,0 +1,14 @@
+#include "create_calculate.h"
+#include <thread>
+using namespace std;
+
+int main() {
+	create_calculate cc;
+	create_calculate::Clock ct;
+	cc.calculate();
+	thread t2(&create_calculate::correct,&cc);
+	t2.detach();
+	thread t1(&create_calculate::Clock::timing,&ct);
+	t1.join();
+	return 0; //返回
+}
