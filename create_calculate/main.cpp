679791c94587a339d97f4f7a662bd5d046be13b1
用户名 <>
2023-03-09 13:03:05 +0800

    modified:   create_calculate/main.cpp

--- create_calculate/main.cpp
+++ create_calculate/main.cpp
@@ -2,13 +2,14 @@
 #include <thread>
 using namespace std;
 
-int main() {
+int main()
+{
 	create_calculate cc;
 	create_calculate::Clock ct;
 	cc.calculate();
-	thread t2(&create_calculate::correct,&cc);
+	thread t2(&create_calculate::correct, &cc);
 	t2.detach();
-	thread t1(&create_calculate::Clock::timing,&ct); //因为是嵌套的类所以有两::
+	thread t1(&create_calculate::Clock::timing, &ct); // 因为是嵌套的类所以有两::
 	t1.join();
-	return 0; //返回
+	return 0; // 返回
 }
